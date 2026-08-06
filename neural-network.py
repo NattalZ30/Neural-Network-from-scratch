a4679733698854ed1e33f9a0c8f09c0554cf9be2
NattalZ30 <nattalz@yahoo.com>
2026-08-06 20:09:44 +0100

    day-3-dense-layers

--- neural-network.py
+++ neural-network.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nnfs
 from nnfs.datasets import spiral_data
+nnfs.init()
 
+##Day 1 - Neurons
+'''
 inputs = [1,2,3,2.5]
 weights = [[0.2, 0.8, -0.5,1],[0.5, -0.91, 0.26,-0.5],[ -0.26, -0.27, 0.17, 0.87]]
 bias = [2,3,1]
@@ -16,8 +19,9 @@ def neural_network(inputs, weights, bias):
 
 output = neural_network(inputs, weights, bias)
 print(output)
-
+'''
 ##DaY 2 - Dot Product
+'''
 a = [1,2,3]
 b = [4,5,6]
 a = np.array([a])
@@ -41,8 +45,27 @@ layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
 print(layer2_outputs)
 
 #########################################
-nnfs.init()
+
 X, y = spiral_data(samples=100, classes=3)
 
 plt.scatter(X[:, 0], X[:, 1], c=y, cmap="brg")
 plt.show()
+'''
+#Day 3 - Dense Layers
+
+class Layer_Dense:
+    def __init__(self,n_inputs, n_neurons):
+        self.weights = 0.01*np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2,3)
+
+dense1.forward(X)
+print(dense1.output[:3])
+##plt.scatter(X[:, 0], X[:, 1], c=y, cmap="brg")
+##plt.show()
